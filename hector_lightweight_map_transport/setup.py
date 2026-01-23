2715ea1cca9f2a10c9e69edc9601d2a322015ee0
Michael Smorto <CyberMerln@gmail.com>
2026-01-23 16:29:58 -0500

    Refractor

--- hector_lightweight_map_transport/setup.py
+++ hector_lightweight_map_transport/setup.py
@@ -1,26 +1,25 @@
 from setuptools import setup
 
-package_name = "hector_lightweight_map_transport"
+package_name = 'hector_lightweight_map_transport'
 
 setup(
     name=package_name,
-    version="0.1.0",
-    packages=[package_name, f"{package_name}.nodes", f"{package_name}.msg"],
+    version='0.1.0',
+    packages=[package_name],
     data_files=[
-        ("share/ament_index/resource_index/packages",
-         [f"resource/{package_name}"]),
-        (f"share/{package_name}", ["package.xml"]),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=["setuptools"],
+    install_requires=['setuptools'],
     zip_safe=True,
-    maintainer="Michael",
-    maintainer_email="you@example.com",
-    description="Lightweight compressed map transport for Hector SLAM over WiFi.",
-    license="BSD",
+    maintainer='Michael',
+    maintainer_email='you@example.com',
+    description='Lightweight map transport utilities (Python + C++).',
+    license='BSD',
     entry_points={
-        "console_scripts": [
-            "map_compressor = hector_lightweight_map_transport.nodes.map_compressor:main",
-            "map_decompressor = hector_lightweight_map_transport.nodes.map_decompressor:main",
+        'console_scripts': [
+            'map_compressor = hector_lightweight_map_transport.map_compressor:main',
         ],
     },
 )
